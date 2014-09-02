062d12baeebe855b84eeb232dd1cd725ee00b2e8
bankst42 <bankst42@gmail.com>
2014-09-02 10:11:55 -0400

    First commit

--- BasicJPanel/src/BasicJPanel.java
+++ BasicJPanel/src/BasicJPanel.java
@@ -28,9 +28,17 @@ public class BasicJPanel extends JPanel{
       made visible or moved or whatever.
     */
     public void paintComponent(Graphics g){
-        g.drawLine(10,10,450,450); // Draw a line from (10,10) to (150,150)
-        g.drawOval(200, 200, 50, 50);
-    }
+        g.drawLine(120, 200, 210, 220); // Draw a line from (10,10) to (150,150)
+        g.drawLine(260, 220, 350, 200);
+        g.drawOval(200, 200, 70, 50); //body
+        g.drawOval(205, 125, 60, 75); //head
+        g.drawOval(175, 250, 120, 100); //bottom
+        g.drawOval(230, 220, 10, 10); //button
+        g.drawOval(230, 270, 10, 10); //button
+        g.drawOval(230, 300, 10, 10); //button
+        g.drawOval(230, 330, 10, 10); //button
+        g.drawOval(220, 150, 10, 10); //eye
+            }
 
     public static void main(String arg[]){
         JFrame frame = new JFrame("BasicJPanel");
